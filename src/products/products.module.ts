f799038a60b56d996556489e536dec8424cdd22a
DannielNavas <dannielnavas@gmail.com>
2023-12-05 06:02:33 -0500

    ✨ Add TypeOrmModule import and Product entity to products module

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { ProductsController } from './controllers/products.controller';
 import { BrandsController } from './controllers/brands.controller';
 import { CategoriesController } from './controllers/categories.controller';
-import { ProductsService } from './services/products.service';
+import { ProductsController } from './controllers/products.controller';
+import { Product } from './entities/product.entity';
 import { BrandsService } from './services/brands.service';
 import { CategoriesService } from './services/categories.service';
+import { ProductsService } from './services/products.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Product])],
   controllers: [ProductsController, CategoriesController, BrandsController],
   providers: [ProductsService, BrandsService, CategoriesService],
   exports: [ProductsService],
