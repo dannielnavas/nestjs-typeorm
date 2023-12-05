d54329fed2265b7f2d1efe32d3b52a5c8de9c954
DannielNavas <dannielnavas@gmail.com>
2023-12-05 06:54:52 -0500

    ✨ Add MySQL configuration to config file

--- src/config.ts
+++ src/config.ts
@@ -13,6 +13,13 @@ export default registerAs('config', () => {
       host: process.env.POSTGRES_HOST,
       port: parseInt(process.env.POSTGRES_PORT, 10),
     },
+    mysql: {
+      password: process.env.MYSQL_ROOT_PASSWORD,
+      user: process.env.MYSQL_USER,
+      db: process.env.MYSQL_DATABASE,
+      host: process.env.MYSQL_HOST,
+      port: parseInt(process.env.MYSQL_PORT, 10),
+    },
     apiKey: process.env.API_KEY,
   };
 });
