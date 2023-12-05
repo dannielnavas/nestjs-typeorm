d3cd3a7c9faed2b1a5c5de9812f997212abc81a5
DannielNavas <dannielnavas@gmail.com>
2023-12-05 06:02:26 -0500

    ✨ Refactor product entity with new columns

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,8 +1,22 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class Product {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ type: 'varchar', length: 255, nullable: false, unique: true })
   name: string;
+
+  @Column({ type: 'text', nullable: false })
   description: string;
+
+  @Column({ type: 'int', nullable: false })
   price: number;
+
+  @Column({ type: 'int', nullable: false })
   stock: number;
+
+  @Column({ type: 'varchar', nullable: false })
   image: string;
 }
