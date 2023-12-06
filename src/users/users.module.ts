c8dc332cd33bf90c7f935e0ba1e7ecdba002b6dc
DannielNavas <dannielnavas@gmail.com>
2023-12-06 07:02:37 -0500

    ✨ Refactor users module imports

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common';
 
 import { CustomerController } from './controllers/customers.controller';
-import { CustomersService } from './services/customers.service';
 import { UsersController } from './controllers/users.controller';
+import { CustomersService } from './services/customers.service';
 import { UsersService } from './services/users.service';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsModule } from '../products/products.module';
+import { Customer } from './entities/customer.entity';
+import { User } from './entities/user.entity';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, TypeOrmModule.forFeature([User, Customer])],
   controllers: [CustomerController, UsersController],
   providers: [CustomersService, UsersService],
 })
