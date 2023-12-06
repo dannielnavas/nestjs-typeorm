90ed4c9371e78b9c8ea20d160e11cb434d3ba7ca
DannielNavas <dannielnavas@gmail.com>
2023-12-06 07:18:15 -0500

    ✨ Refactor brand entity and brands service

--- src/products/entities/brand.entity.ts
+++ src/products/entities/brand.entity.ts
@@ -1,5 +1,28 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Product } from './product.entity';
+
+@Entity()
 export class Brand {
+  @PrimaryGeneratedColumn()
   id: number;
+  @Column({ type: 'varchar', length: 255, nullable: false, unique: true })
   name: string;
+  @Column({ type: 'text', nullable: false })
   image: string;
+  @CreateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
+  createAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
+  updateAt: Date;
+
+  // referencia a la entidad Product que lleva la realación real con la tabla
+  @OneToMany(() => Product, (product) => product.brand)
+  products: Product[];
 }
