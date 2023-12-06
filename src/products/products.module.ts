3ec4521f39c8a65ab267c313b88195ace9261cbd
DannielNavas <dannielnavas@gmail.com>
2023-12-06 07:18:33 -0500

    ✨ Add Brand entity to products module

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -4,13 +4,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { BrandsController } from './controllers/brands.controller';
 import { CategoriesController } from './controllers/categories.controller';
 import { ProductsController } from './controllers/products.controller';
+import { Brand } from './entities/brand.entity';
 import { Product } from './entities/product.entity';
 import { BrandsService } from './services/brands.service';
 import { CategoriesService } from './services/categories.service';
 import { ProductsService } from './services/products.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product])],
+  imports: [TypeOrmModule.forFeature([Product, Brand])],
   controllers: [ProductsController, CategoriesController, BrandsController],
   providers: [ProductsService, BrandsService, CategoriesService],
   exports: [ProductsService],
