4bae2e97895a6111607c43c8074969927be5c6eb
DannielNavas <dannielnavas@gmail.com>
2023-12-07 06:22:23 -0500

    ✨ Add Category entity to products module

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -5,13 +5,14 @@ import { BrandsController } from './controllers/brands.controller';
 import { CategoriesController } from './controllers/categories.controller';
 import { ProductsController } from './controllers/products.controller';
 import { Brand } from './entities/brand.entity';
+import { Category } from './entities/category.entity';
 import { Product } from './entities/product.entity';
 import { BrandsService } from './services/brands.service';
 import { CategoriesService } from './services/categories.service';
 import { ProductsService } from './services/products.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product, Brand])],
+  imports: [TypeOrmModule.forFeature([Product, Brand, Category])],
   controllers: [ProductsController, CategoriesController, BrandsController],
   providers: [ProductsService, BrandsService, CategoriesService],
   exports: [ProductsService],
