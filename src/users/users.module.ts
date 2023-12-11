5e86ee22792eb0b85df51a1ac61b2dce4cecb8b0
Danniel Navas <dannielnavas@gmail.com>
2023-12-11 06:35:46 -0500

    ✨ Add order and order item controllers and services to users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -7,17 +7,26 @@ import { UsersService } from './services/users.service';
 
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsModule } from '../products/products.module';
+import { OrderItemController } from './controllers/order-item.controller';
+import { OrdersController } from './controllers/orders.controller';
 import { Customer } from './entities/customer.entity';
 import { OrderItem } from './entities/order-item.entity';
 import { Order } from './entities/order.entity';
 import { User } from './entities/user.entity';
+import { OrderItemService } from './services/order-item.service';
+import { OrdersService } from './services/orders.service';
 
 @Module({
   imports: [
     ProductsModule,
     TypeOrmModule.forFeature([User, Customer, Order, OrderItem]),
   ],
-  controllers: [CustomerController, UsersController],
-  providers: [CustomersService, UsersService],
+  controllers: [
+    CustomerController,
+    UsersController,
+    OrdersController,
+    OrderItemController,
+  ],
+  providers: [CustomersService, UsersService, OrdersService, OrderItemService],
 })
 export class UsersModule {}
